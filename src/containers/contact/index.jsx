0a42b9867c627001a452bed319a52dd16dca006f
ramanujm@yahoo.com <ramanujm@yahoo.com>
2025-06-23 11:17:52 -0400

    email integration

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
 import "./styles.scss";
+import emailjs from "@emailjs/browser";
+
+// Replace with your EmailJS service ID, template ID, and public key
+const YOUR_SERVICE_ID = "service_3yvxm0u"; // Replace with your EmailJS service ID
+const YOUR_TEMPLATE_ID = "template_1trkrij"; // Replace with your EmailJS template ID
+const YOUR_PUBLIC_KEY = "e936Lf3I6amsoEW12"; // Replace with your EmailJS public key
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, {
+        publicKey: YOUR_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <section id="contact" className="contact">
       <PageHeaderContent
@@ -12,40 +37,47 @@ const Contact = () => {
       />
       <div className="contact__content">
         <h3 className="contact__content__header-text">Get in Touch</h3>
-        <div className="contact__content__form">
-          <div className="contact__content__form__controlswrapper">
-            <div>
-              <input required name="name" className="inputName" type={"text"} />
-              <label htmlFor="name" className="nameLabel">
-                Name
-              </label>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="contact__content__form">
+            <div className="contact__content__form__controlswrapper">
+              <div>
+                <input
+                  required
+                  name="name"
+                  className="inputName"
+                  type={"text"}
+                />
+                <label htmlFor="name" className="nameLabel">
+                  Name
+                </label>
+              </div>
+              <div>
+                <input
+                  required
+                  name="email"
+                  className="inputEmail"
+                  type={"text"}
+                />
+                <label htmlFor="email" className="emailLabel">
+                  Email
+                </label>
+              </div>
+              <div>
+                <textarea
+                  required
+                  name="message"
+                  className="inputDescription"
+                  type={"text"}
+                  rows="10"
+                />
+                <label htmlFor="message" className="descriptionLabel">
+                  Message
+                </label>
+              </div>
             </div>
-            <div>
-              <input
-                required
-                name="email"
-                className="inputEmail"
-                type={"text"}
-              />
-              <label htmlFor="email" className="emailLabel">
-                Email
-              </label>
-            </div>
-            <div>
-              <textarea
-                required
-                name="description"
-                className="inputDescription"
-                type={"text"}
-                rows="10"
-              />
-              <label htmlFor="description" className="descriptionLabel">
-                Description
-              </label>
-            </div>
+            <button type="submit">Send</button>
           </div>
-          <button>Submit</button>
-        </div>
+        </form>
       </div>
     </section>
   );
